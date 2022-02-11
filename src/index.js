6003b2f7e8a4f4dc440799fb8dcc480aeffef60d
JonatSrG <JonatSrG>
2022-02-11 12:25:02 -0600

    empezando a conectar la api weatherMao

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
-console.log('Happy hacking :)')
+let weather = {
+    apiKey: "ecf45c7a8aaf72ca8c300045e46763b6";
+}
